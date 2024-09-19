b65bd3db2e0f48ed1b82c10f7da60910943baa13
Ryan Young <rayoung1093@gmail.com>
2024-09-18 22:34:34 -0400

    test relative paths

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from api.test_fun import run
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello World", "python": run()}
